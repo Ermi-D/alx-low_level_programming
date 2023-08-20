87dc3e2d51157c222dedfb935d80df2120bbaeb5
ErmiD <root@DESKTOP-M4R2VJK.localdomain>
2023-08-20 04:19:59 +0300

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,16 +4,19 @@
  *@s: the memory area pointed to be filled
  *@b:constant byte
  *@n:number of bytes to be changed
+
  * Return:apointer to the memory area (s)
+*
 */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 		int i = 0;
 
 			for (; n > 0; i++)
 					{
-								s[i] = b;
-										n--;
-											}
-				return (s);
+				s[i] = b;
+			 	n--;
+	}
+	return (s);
 }
