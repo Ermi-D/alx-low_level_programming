289ba9ee6e3b663d4bcb33f03374167935760719
Ermi-D <ermidinka@gmail.com>
2023-08-31 14:35:22 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - print string;
+ * @s: string to print;
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\n')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
