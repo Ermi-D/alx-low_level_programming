4474296c9b2e7ed839a17b53bb26bef863db6be0
Ermi-D <ermidinka@gmail.com>
2023-09-01 14:03:50 -0700

    update

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memset - function fills the first n bytes of the memory area
+ *@s: the memory area pointed to be filled
+ *@b:constant byte
+ *@n:number of bytes to be changed
+ *
+* Return: a pointer to memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
