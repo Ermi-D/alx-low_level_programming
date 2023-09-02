43d9038d765833b9c9c7e4175fabb9ff801d7371
Ermi-D <ermidinka@gmail.com>
2023-09-02 14:08:38 -0700

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - print the number of arguments 
+ * main - print the number of arguments
  * @argc: number of arguments
  * @argv: arguments vector
- * Return:  0 
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-(void) argv; 
-	printf("%d\n", argc - 1);
+(void) argv;
+printf("%d\n", argc - 1);
 
 	return (0);
 }
-
