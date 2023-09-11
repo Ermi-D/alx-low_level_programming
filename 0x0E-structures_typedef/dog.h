bdb3730cb150d4e5bf1c30fecc6828e0706c12da
Ermi-D <ermidinka@gmail.com>
2023-09-11 11:44:55 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog
+ * @age: dog age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
